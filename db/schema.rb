0f91cde0b0e513a1b1777a91127da1b0f5ac5e87
FDF <theopulus@gmail.com>
2017-09-17 00:51:16 +0200

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170915235005) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pinchos", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false

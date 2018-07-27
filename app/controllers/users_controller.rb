7c90e4e26fe05deddbcc46d4971705f685d83be4
FdF <theopulus@gmail.com>
2018-07-27 13:55:37 +0200

    reindented users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:notice] = "Usuario dado de alta con éxito"
-            flash[:color] = "valid"
-        else
-            flash[:notice] = "Los datos son inválidos"
-            flash[:color] = "invalid"
-        end
-        render "new"
-    end
+  def new
+    @user = User.new
+  end
 
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :name, :password, :password_confirmation)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'Usuario dado de alta con éxito'
+      flash[:color] = 'valid'
+    else
+      flash[:notice] = 'Los datos son inválidos'
+      flash[:color] = 'invalid'
     end
+    render 'new'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :password, :password_confirmation)
+  end
 end

41b94f2c7d9b0f361c219f358b39419cd63e77d5
FdF <theopulus@gmail.com>
2018-07-27 13:55:16 +0200

    reindented users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,26 @@
 require 'bcrypt'
 class User < ApplicationRecord
-    has_many :votes
-    has_many :pinchos, through: :votes
+  has_many :votes
+  has_many :pinchos, through: :votes
 
-    attr_accessor :password
+  attr_accessor :password
 
-    validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
-    validates :name, :presence => true, :length => { :in => 3..100 }
-    validates :password, :confirmation => true
-    validates_length_of :password, :in => 6..20, :on => create
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :name, :presence => true, :length => { :in => 3..100 }
+  validates :password, :confirmation => true
+  validates_length_of :password, :in => 6..20, :on => create
 
-    before_save :encrypt_password
-    after_save :clear_password
+  before_save :encrypt_password
+  after_save :clear_password
 
-    def encrypt_password
-        if password.present?
-            self.salt = BCrypt::Engine.generate_salt
-            self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
-        end
-    end
+  def encrypt_password
+    return unless password.present?
 
-    def clear_password
-        self.password = nil
-    end
+    self.salt = BCrypt::Engine.generate_salt
+    self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
+  end
+
+  def clear_password
+    self.password = nil
+  end
 end

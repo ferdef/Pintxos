19fb43973a7300eb82e94b053b9783f7c97c3fdf
F3RD3F <f3rd3f@gmail.com>
2021-12-28 22:49:21 +0100

    Pintxos admin

--- pintxos/admin.py
+++ pintxos/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Pintxo, PintxoVote
 
-admin.site.register(Pintxo)
-admin.site.register(PintxoVote)
+@admin.register(Pintxo)
+class PintxoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator', 'contest')
+    list_filter = ('contest', 'creator')
+
+@admin.register(PintxoVote)
+class PintxoVoteAdmin(admin.ModelAdmin):
+    list_display = ('pintxo', 'voter', 'value')
+    list_filter = ('pintxo', 'voter', 'pintxo__contest')

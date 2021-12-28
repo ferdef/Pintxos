e62837c7f936e8ca9f6773232741c775ab267e31
F3RD3F <f3rd3f@gmail.com>
2021-12-28 13:57:48 +0100

    contest has a model and it's manageable from admin

--- contests/models.py
+++ contests/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Contest(models.Model):
+    contest_date = models.DateField()
+
+    def __str__(self):
+        return  self.contest_date.strftime("%d-%b-%Y")
--- wines/admin.py
+++ wines/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
-from .models import Contest
-
 # Register your models here.
-admin.site.register(Contest)

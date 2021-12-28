a9867ddd024745bea61ec3d43b0ba2af5c6daf69
F3RD3F <f3rd3f@gmail.com>
2021-12-28 16:55:38 +0100

    forgot to add wines model

--- wines/models.py
+++ wines/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from contests.models import Contest
 
 # Create your models here.
+class Wine(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField(validators=[MaxValueValidator(2022), MinValueValidator(1960)])
+    pdo = models.CharField(max_length=64)
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.name} - {self.creator.get_username()} - Contest:{self.contest}"

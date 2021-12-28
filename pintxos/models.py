17ad7dd259299e7ca4b79b012c1a5deda50cf93d
F3RD3F <f3rd3f@gmail.com>
2021-12-28 14:11:34 +0100

    added pintxo model

--- pintxos/models.py
+++ pintxos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from contests.models import Contest
 
 # Create your models here.
+class Pintxo(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.creator.get_username()} - Contest:{self.contest}"
--- pintxoscontest/settings.py
+++ pintxoscontest/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'contests.apps.ContestsConfig',
+    'pintxos.apps.PintxosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

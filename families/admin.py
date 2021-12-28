1208d8c54a1f736739058831b6684e0b3692f99d
F3RD3F <f3rd3f@gmail.com>
2021-12-28 17:28:19 +0100

    families now show members in admin

--- families/admin.py
+++ families/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Profile, Family
 
-admin.site.register(Profile)
-admin.site.register(Family)
+class ProfileInline(admin.TabularInline):
+    model = Profile
+
+@admin.register(Family)
+class FamilyAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline]
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+

220e2e86f343850a9fa958db3c584c305d12040b
F3RD3F <f3rd3f@gmail.com>
2021-12-28 17:09:26 +0100

    added family

--- pintxoscontest/settings.py
+++ pintxoscontest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'families.apps.FamiliesConfig',
     'contests.apps.ContestsConfig',
     'pintxos.apps.PintxosConfig',
     'wines.apps.WinesConfig',

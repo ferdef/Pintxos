5996e5ece03021c30e118389ebad80b103360390
Fernando de Francisco <fernando.defrancisco@unity3d.com>
2023-04-24 17:59:13 +0200

    Added a form to create contests

--- Data/DBService.cs
+++ Data/DBService.cs
@@ -15,10 +15,28 @@ public class DBService
         return _db.Users.OrderBy(o => o.Id).ToList();
     }
 
+    #region Contests
     public List<Contest> GetContests()
     {
         return _db.Contests.OrderByDescending(o => o.Date).ToList();
     }
+
+    public void NewContest(Contest newContest)
+    {
+        _db.Contests.Add(newContest);
+        _db.SaveChangesAsync();
+    }
+
+    public void EditContest(int id, Contest newData)
+    {
+        var entity = _db.Contests.Find(id);
+        if (entity != null)
+        {
+            _db.Contests.Entry(entity).CurrentValues.SetValues(newData);
+        }
+    }
+
+    #endregion
 
     public List<Pintxo> GetPintxos()
     {

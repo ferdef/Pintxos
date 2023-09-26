1ccba8d4797309f7d19656e39af140c6f13be006
Ferdef <f3rd3f@gmail.com>
2023-09-26 18:38:35 +0200

    adding some test views

--- contest/views.py
+++ contest/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Contest, Pintxo, Vote
+
+def index(request):
+    return HttpResponse("Hello Contests")
+
+def pintxos(request):
+    pintxos_list = Pintxo.objects.all()
+    template = loader.get_template("pintxo/index.html")
+    context = {
+        "pintxos_list": pintxos_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def contest_detail(request, contest_id):
+    return HttpResponse(f"Looking for contest {contest_id}")
+
+def pintxo_detail(request, pintxo_id):
+    return HttpResponse(f"Looking for pintxo {pintxo_id}")

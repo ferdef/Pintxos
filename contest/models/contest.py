cb258bf4d6c691b83571188da0a1390a0780f15a
F3rd3f <f3rd3f@gmail.com>
2023-10-09 14:23:02 +0200

    added active field on contests

--- contest/models/contest.py
+++ contest/models/contest.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Contest(models.Model):
     contest_date = models.DateField("Contest Date")
+    active = models.BooleanField("Active?", default=False)
 
     def __unicode__(self):
         return f"Contest on {self.contest_date}"
@@ -9,5 +10,8 @@ class Contest(models.Model):
     def __str__(self):
         return f"Contest on {self.contest_date}"
 
-    def clean_contest(self):
-        pass
+    def set_active(self):
+        contests = Contest.objects.all()
+        for contest in contests:
+            contest.active = False
+        self.active = True

9d605e3ec4ab7a7eaf00a686a56d39b27d1c2e5a
F3rd3f <f3rd3f@gmail.com>
2023-10-10 12:14:01 +0200

    owner is no longer required

--- contest/models/pintxo.py
+++ contest/models/pintxo.py
@@ -5,11 +5,14 @@ from . import Contest
 class Pintxo(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=2048, blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
 
     def __unicode__(self):
-        return f"{self.name} by {self.owner.get_full_name()}"
+        return f"{self.name} by {self.get_owner_name()}"
     
     def __str__(self):
-        return f"{self.name} by {self.owner.get_full_name()}"
+        return f"{self.name} by {self.get_owner_name()}"
+
+    def get_owner_name(self):
+        return self.owner.get_full_name() if self.owner else "Unknown"

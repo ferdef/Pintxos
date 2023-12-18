4e8bc4915aa73f804bdd310d9d6f302dd53bc21c
Ferdef <f3rd3f@gmail.com>
2023-12-18 10:47:57 +0100

    Added Static serving and landing basic page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Adding Statics
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Adding landing page
+	mux.HandleFunc("/", home)
+
 	err := http.ListenAndServe(":4001", mux)
 	log.Fatal(err)
 }
